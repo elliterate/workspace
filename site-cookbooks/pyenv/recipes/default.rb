10acee7ed57a5a22977238cea1f473e3d15a6d41
Ian Lesperance <ian@elliterate.com>
2017-12-21 23:32:56 +0000

    Install python-build dependencies

--- site-cookbooks/pyenv/recipes/default.rb
+++ site-cookbooks/pyenv/recipes/default.rb
@@ -20,3 +20,17 @@ execute "Copying pyenv's .git to #{node['pyenv']['dir']}" do
   command "rsync -axSH #{Chef::Config[:file_cache_path]}/pyenv/ #{node['pyenv']['dir']}"
   user node['workstation']['user']
 end
+
+# Install python-build dependencies
+package 'curl'
+package 'libbz2-dev'
+package 'libncurses5-dev'
+package 'libncursesw5-dev'
+package 'libreadline-dev'
+package 'libsqlite3-dev'
+package 'libssl-dev'
+package 'llvm'
+package 'tk-dev'
+package 'wget'
+package 'xz-utils'
+package 'zlib1g-dev'
