113066f53fbc153d87b37893c872ef667cbb0408
Ian Lesperance <ian@elliterate.com>
2017-12-22 00:18:58 +0000

    Install Ruby compilation dependencies

--- site-cookbooks/rbenv/recipes/ruby-build.rb
+++ site-cookbooks/rbenv/recipes/ruby-build.rb
@@ -17,3 +17,16 @@ execute "Copying ruby-build's .git to #{node['ruby-build']['dir']}" do
   command "rsync -axSH #{Chef::Config[:file_cache_path]}/ruby-build/ #{node['ruby-build']['dir']}"
   user node['workstation']['user']
 end
+
+# Compilation dependencies
+package 'autoconf'
+package 'bison'
+package 'build-essential'
+package 'libffi-dev'
+package 'libgdbm-dev'
+package 'libgdbm3'
+package 'libncurses5-dev'
+package 'libreadline6-dev'
+package 'libssl-dev'
+package 'libyaml-dev'
+package 'zlib1g-dev'
